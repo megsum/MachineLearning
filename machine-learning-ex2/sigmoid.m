546274a6cfa59af1bc48f838dc9b6ba829808310
Megan Sumner <msumner@ualberta.ca>
2022-05-24 12:34:48 -0600

    Completing first programming assignmnet

--- machine-learning-ex2/sigmoid.m
+++ machine-learning-ex2/sigmoid.m
@@ -9,10 +9,12 @@ g = zeros(size(z));
 % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
 
+% The sigmoid function is defined as g(z) = 1 / (1 + e^-z)
+% First, populate a matrix the size of z with e
+e_var = ones(size(z));
+e_var = e * e_var;
 
-
-
-
+g = 1 ./ (1 + e_var.**-z)
 % =============================================================
 
 end
